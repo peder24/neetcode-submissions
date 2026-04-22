c519eb5d5009a3ad3ade639abbbd563784480503
peder24 <151603615+peder24@users.noreply.github.com>
2026-04-22 17:04:21 +0700

    Add: anagram-groups - submission-27

---
+++ Data Structures & Algorithms/anagram-groups/submission-27.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const res = {}
+
+        for (const s of strs) {
+            let code = new Array(26).fill(0)
+            for (const c of s) {
+                code[c.charCodeAt(0) - 'a'.charCodeAt(0)] += 1
+            }
+
+            if (!(code in res)) {
+                res[code] = []
+            }
+
+            res[code].push(s)
+        }
+        return Object.values(res)
+    }
+}
