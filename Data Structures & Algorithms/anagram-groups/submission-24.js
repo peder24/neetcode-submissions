abb42dfc505dce04bab51c6788b7c9965e154eb5
peder24 <151603615+peder24@users.noreply.github.com>
2026-04-22 11:52:38 +0700

    Add: anagram-groups - submission-24

---
+++ Data Structures & Algorithms/anagram-groups/submission-24.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const res = new Map()
+
+        for (const s of strs) {
+            let code = new Array(26).fill(0)
+            for (const c of s) {
+                code[c.charCodeAt(0) - 'a'.charCodeAt(0)] += 1
+            }
+
+            let key = code.join('#')
+
+            if (!(res.has(key))) {
+                res.set(key, [])
+            }
+            res.get(key).push(s)
+        }
+        return Array.from(res.values())
+    }
+}
