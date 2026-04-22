a8c45ce733ab02958e45d4aa52ec321494484278
peder24 <151603615+peder24@users.noreply.github.com>
2026-04-22 19:56:44 +0700

    Add: permutation-string - submission-12

---
+++ Data Structures & Algorithms/permutation-string/submission-12.js
@@ -0,0 +1,52 @@
+class Solution {
+    /**
+     * @param {string} s1
+     * @param {string} s2
+     * @return {boolean}
+     */
+    checkInclusion(s1, s2) {
+        if (s1.length > s2.length) {
+            return false
+        }
+
+        const counts1 = new Array(26).fill(0), counts2 = new Array(26).fill(0)
+        for (let i = 0; i < s1.length; i++) {
+            counts1[s1[i].charCodeAt(0) - 'a'.charCodeAt(0)] += 1
+            counts2[s2[i].charCodeAt(0) - 'a'.charCodeAt(0)] += 1
+        }
+
+        let match = 0
+        for (let i = 0; i < 26; i++) {
+            if (counts1[i] === counts2[i]) {
+                match += 1
+            } else {
+                match += 0
+            }
+        }
+
+        let l = 0
+        for (let r = s1.length; r < s2.length; r++) {
+            if (match == 26) {
+                return true
+            }
+
+            let index = s2[r].charCodeAt(0) - 'a'.charCodeAt(0)
+            counts2[index] += 1
+            if (counts1[index] == counts2[index]) {
+                match += 1
+            } else if (counts1[index] + 1 == counts2[index]) {
+                match -= 1
+            }
+
+            index = s2[l].charCodeAt(0) - 'a'.charCodeAt(0)
+            counts2[index] -= 1
+            if (counts1[index] == counts2[index]) {
+                match += 1
+            } else if (counts1[index] - 1 == counts2[index]) {
+                match -= 1
+            }
+            l++
+        }
+        return match == 26
+    }
+}
