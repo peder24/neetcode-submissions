83248421753b38af04a4970ebb9e90ddce306ee1
peder24 <151603615+peder24@users.noreply.github.com>
2026-04-22 23:42:23 +0700

    Add: top-k-elements-in-list - submission-41

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-41.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const count = {}
+        for (let i = 0; i < nums.length; i++) {
+            count[nums[i]] = (count[nums[i]] || 0) + 1
+        }
+
+        const arr = Object.entries(count).map(([num, freq]) => [freq, parseInt(num)])
+        arr.sort((a, b) => b[0] - a[0])
+        
+        const res = []
+        for (let i = 0; i < k; i++) {
+            res.push(arr[i][1])
+        }
+        return res
+    }
+}
