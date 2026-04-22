70004f5b60ead4bc4dbf069caefff8116822dac5
peder24 <151603615+peder24@users.noreply.github.com>
2026-04-22 11:24:54 +0700

    Add: anagram-groups - submission-23

---
+++ Data Structures & Algorithms/anagram-groups/submission-23.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const res = {}
+        for (const s of strs) {
+            let sortedS = s.split("")
+                            .sort((a, b) => a.charCodeAt(0) - b.charCodeAt(0))
+                            .join("")
+            
+            if (!(sortedS in res)) {
+                res[sortedS] = []
+            }
+
+            res[sortedS].push(s)
+        }
+        return Object.values(res)
+    }
+}
