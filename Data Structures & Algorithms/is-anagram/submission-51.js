44c0a8044befabb9f1aa0368e8673717f7c4a5bd
peder24 <151603615+peder24@users.noreply.github.com>
2026-04-26 21:49:40 +0700

    Add: is-anagram - submission-51

---
+++ Data Structures & Algorithms/is-anagram/submission-51.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length != t.length) {
+            return false
+        }
+
+        const count = new Array(26).fill(0)
+        for (let i = 0; i < s.length; i++) {
+            count[s[i].charCodeAt(0) - 'a'.charCodeAt(0)] += 1
+            count[t[i].charCodeAt(0) - 'a'.charCodeAt(0)] -= 1
+        }
+
+        for (const v of count) {
+            if (v != 0) {
+                return false
+            }
+        }
+        return true
+    }
+}
