db757d1df3f9e9f224bdf46b92b386cf33e71bc4
peder24 <151603615+peder24@users.noreply.github.com>
2026-04-27 17:06:03 +0700

    Add: products-of-array-discluding-self - submission-14

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-14.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        const res = new Array(nums.length).fill(1)
+
+        let prefix = 1
+        for (let i = 0; i < nums.length; i++) {
+            res[i] = prefix
+            prefix *= nums[i]
+        }
+
+        let suffix = 1
+        for (let i = nums.length - 1; i >= 0; i--) {
+            res[i] *= suffix
+            suffix *= nums[i]
+        }
+        return res
+    }
+}
