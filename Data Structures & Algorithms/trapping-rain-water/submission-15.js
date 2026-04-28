6e26b8c392f492d12eea38852c35978d1bc9c44d
peder24 <151603615+peder24@users.noreply.github.com>
2026-04-28 16:10:39 +0700

    Add: trapping-rain-water - submission-15

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-15.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height) {
+        let l = 0, r = height.length - 1
+        let maxL = height[l], maxR = height[r]
+        let res = 0
+
+        while (l < r) {
+            if (maxL <= maxR) {
+                l++
+                maxL = Math.max(maxL, height[l])
+                res += maxL - height[l]
+            } else {
+                r--
+                maxR = Math.max(maxR, height[r])
+                res += maxR - height[r]
+            }
+        }
+        return res
+    }
+}
