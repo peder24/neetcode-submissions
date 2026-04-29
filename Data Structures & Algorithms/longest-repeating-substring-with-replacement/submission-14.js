302cb5d19c61f46c308383d9f5cc7106ee012680
peder24 <151603615+peder24@users.noreply.github.com>
2026-04-29 20:14:56 +0700

    Add: longest-repeating-substring-with-replacement - submission-14

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-14.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        const count = {}
+        let longest = 0
+        let maxf = 0
+        let l = 0, r = 0
+
+        while (r < s.length) {
+            count[s[r]] = (count[s[r]] || 0) + 1
+            maxf = Math.max(maxf, count[s[r]])
+
+            while ((r - l + 1) - maxf > k) {
+                count[s[l]] -= 1
+                l++
+            }
+
+            longest = Math.max(longest, r - l + 1)
+            r++
+        }
+        return longest
+    }
+}
