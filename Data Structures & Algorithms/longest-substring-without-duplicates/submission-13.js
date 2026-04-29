fc639d4746159a9580e4a4a6f38cccb25a6af822
peder24 <151603615+peder24@users.noreply.github.com>
2026-04-29 14:34:36 +0700

    Add: longest-substring-without-duplicates - submission-13

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-13.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        let longest = 0
+        let l = 0, r = 0
+        const count = new Set()
+
+        while (r < s.length) {
+            while (count.has(s[r])) {
+                count.delete(s[l])
+                l++
+            }
+            count.add(s[r])
+            longest = Math.max(longest, r - l + 1)
+            r++
+        }
+        return longest
+    }
+}
