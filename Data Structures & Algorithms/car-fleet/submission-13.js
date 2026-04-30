ca298bfd18b59f30d450ab396241304daa0897b9
peder24 <151603615+peder24@users.noreply.github.com>
2026-04-30 22:44:00 +0700

    Add: car-fleet - submission-13

---
+++ Data Structures & Algorithms/car-fleet/submission-13.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target, position, speed) {
+        const pair = position.map((p, i) => [p, speed[i]])
+        pair.sort((a, b) => b[0] - a[0])
+        const stack = []
+
+        for (const [p, s] of pair) {
+            stack.push((target - p)/s)
+            while (stack.length >= 2 && stack[stack.length - 2] >= stack[stack.length - 1]) {
+                stack.pop()
+            }
+        }
+        return stack.length
+    }
+}
