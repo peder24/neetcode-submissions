875f7268f559b7d3c108f8f635a19eb010ce01f7
peder24 <151603615+peder24@users.noreply.github.com>
2026-05-01 15:52:10 +0700

    Add: search-2d-matrix - submission-6

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-6.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        let ROWS = matrix.length, COLS = matrix[0].length
+        let top = 0, bot = ROWS - 1
+
+        while (top <= bot) {
+            let mid = top + parseInt((bot - top)/2)
+            if (target < matrix[mid][0]) {
+                bot = mid - 1
+            } else if (target > matrix[mid][COLS - 1]) {
+                top = mid + 1
+            } else {
+                break
+            }
+        }
+
+        if (top > bot) {
+            return false
+        }
+
+        let row = top + parseInt((bot - top)/2)
+        let l = 0, r = COLS - 1
+        while (l <= r) {
+            let mid = l + parseInt((r - l)/2)
+            if (target < matrix[row][mid]) {
+                r = mid - 1
+            } else if (target > matrix[row][mid]) {
+                l = mid + 1
+            } else {
+                return true
+            }
+        }
+        return false
+    }
+}
