14558a5c69fb9419b7173f1444a7924f4ac7a303
peder24 <151603615+peder24@users.noreply.github.com>
2026-05-15 11:27:13 +0700

    Add: two-integer-sum - submission-61

---
+++ Data Structures & Algorithms/two-integer-sum/submission-61.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const seen = {}
+
+        for (let i = 0; i < nums.length; i++) {
+            let diff = target - nums[i]
+            if(diff in seen) {
+                return [seen[diff], i]
+            }
+            seen[nums[i]] = i
+        }
+    }
+}
