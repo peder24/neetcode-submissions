23cdc2f670138d51f9572307785c7abd3cfb48cd
peder24 <151603615+peder24@users.noreply.github.com>
2026-06-08 10:14:47 +0700

    Add: is-anagram - submission-72

---
+++ Data Structures & Algorithms/is-anagram/submission-72.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length) {
+            return false
+        }
+
+        const countS = new Map(), countT = new Map()
+        for (let i = 0; i < s.length; i++) {
+            countS.set(s[i], (countS.get(s[i]) || 0) + 1)
+            countT.set(t[i], (countT.get(t[i]) || 0) + 1)
+        }
+
+        for (const [key, value] of Array.from(countS)) {
+            if (value !== countT.get(key)) {
+                return false
+            }
+        }
+        return true
+    }
+}
